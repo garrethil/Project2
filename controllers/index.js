a172c8c823733060a02a8f357f105aee27035990
Daniel John <dcampo85@gmail.com>
2024-03-04 19:19:20 -0500

    index.js controllers

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+// imports express router pkg + define routes for the app
+const router = require("express").Router;
+
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+module.exports = router;
