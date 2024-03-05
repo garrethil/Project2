94c1a0c842b0a3faf805109816935c5eff8f102a
Daniel John <dcampo85@gmail.com>
2024-03-04 20:45:02 -0500

    server.js updates

---
+++ server.js
@@ -0,0 +1,14 @@
+// import modules
+const path = require("path");
+const express = require("express");
+const exhbs = require("exoress-handlebars");
+const routes = require("./controllers");
+const sequelize = require("./config/connection.js");
+
+const app = express();
+const PORT = processs.env.PORT || 3001;
+
+const hbs = exhbs.create();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
