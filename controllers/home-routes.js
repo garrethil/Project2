539af11035eacdff9b3d50be049b1fe9f9b5ad6c
Daniel John <dcampo85@gmail.com>
2024-03-04 19:35:02 -0500

    add logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 
-// get method for homepage views
+// get method for homepage views, login, signup + logout
 router.get("/", async (req, res) => {});
 
 router.get("/login", async (req, res) => {});
 
-router.get("/signUp", async (req, res) => {});
+router.get("/signup", async (req, res) => {});
+
+router.get("/logout", async (req, res) => {});
