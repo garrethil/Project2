48c09a21cc98e7624e1feff8ee8b81ad3b7b0a74
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-05 17:10:35 -0700

    Making the connection to the sequelize database

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ const hbs = exhbs.create();
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+sequelize.sycn({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening on port", PORT));
+});
