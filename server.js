55051d849c866616ffdf674bbb01a57dcf21fa7a
Daniel John <dcampo85@gmail.com>
2024-03-05 19:13:18 -0500

    add middlewares

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ const hbs = exhbs.create();
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Go ahead, user. I'm listening"));
+});
