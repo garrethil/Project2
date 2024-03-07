496b137dbf8cb78613e7d2c204e7de01797adc75
MunibaP <munibapervez596@gmail.com>
2024-03-07 18:59:32 -0500

    Updated email column

--- models/User.js
+++ models/User.js
@@ -19,7 +19,14 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
             unique: true,
+            validate: {
+                isEmail: true,
+            },
         },
         password: {
             type: DataTypes.STRING,
