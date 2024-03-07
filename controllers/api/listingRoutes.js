0b32e7a7d600c130fa627a1290351a6d579f9e25
garrethil <garrethildebrandt@gmail.com>
2024-03-07 13:01:22 -0500

    add apiRoutes files

---
+++ controllers/api/listingRoutes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const { User, Listing, City, BuildingType, ListingType } = require("../../models");
+
+// get all Listings
+
+router.get("/", async (req, res) => {
+    try {
+        const listingData = await Listing.findAll({
+            include: [{ model: City, ListingType, BuildingType, User }]
+        });
+        res.status(200).json(listingData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// get listing by id
+router.get("/:id", async (req, res) => {
+    try {
+      const listingData = await Listing.findByPk(req.params.id, {
+        include: [{ model: City, ListingType, BuildingType, User }]
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: "No listing found with that id!"})
+        return;
+      }
+  
+      res.status(200).json(listingData);
+    } catch (err) {
+      res.status(500).json(err);
+      
+    }
+  });
