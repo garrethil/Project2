21f478cb1ce7a891fae6b1db7624aa4442da4551
garrethil <garrethildebrandt@gmail.com>
2024-03-07 19:47:49 -0500

    move get routes to home-routes.js

--- controllers/api/cityRoutes.js
+++ controllers/api/cityRoutes.js
@@ -1,17 +1,4 @@
 const router = require("express").Router();
 const { User, Listing, City, BuildingType, ListingType } = require("../../models");
 
-//get all cities to select a city of choice when making a listing or browsing listings
-
-get.router("/", async (req, res) => {
-    try {
-        const cityData = await City.findAll()   
-    
-        res.status(200).json(cityData);
-    } 
-        catch (err) {
-            res.status(500).json(err);
-    }
-})
-
 module.exports = router;
--- controllers/api/listingRoutes.js
+++ controllers/api/listingRoutes.js
@@ -1,39 +1,43 @@
 const router = require("express").Router();
 const { User, Listing, City, BuildingType, ListingType } = require("../../models");
-
-// get all Listings
-
-router.get("/", async (req, res) => {
-    try {
-        const listingData = await Listing.findAll({
-            include: [{ model: City, ListingType, BuildingType, User }]
-        });
-        res.status(200).json(listingData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-})
-
-// get listing by id
-router.get("/:id", async (req, res) => {
-    try {
-      const listingData = await Listing.findByPk(req.params.id, {
-        include: [{ model: City, ListingType, BuildingType, User }]
-      });
-  
-      if (!listingData) {
-        res.status(404).json({ message: "No listing found with that id!"})
-        return;
-      }
-  
-      res.status(200).json(listingData);
-    } catch (err) {
-      res.status(500).json(err);
-      
-    }
-  });
+const withAuth = require("../../utils");
 
 // delete Listing by ID
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const listingData = await Listing.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!listingData) {
+      res.status(404).json({ message: 'No listing found with this id!' });
+      return;
+    }
+
+    res.status(200).json(listingData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// new listing
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newListings = await Listing.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
 
 
 
