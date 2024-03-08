617496a4ade7ecc2013202df72614a7576cdb11b
MunibaP <munibapervez596@gmail.com>
2024-03-07 20:49:11 -0500

    Updated code on listingData and userData

--- seeds/userData.js
+++ seeds/userData.js
@@ -7,6 +7,21 @@ const userData = [
         email: 'john_doe@example.com',
         pswd: bcrypt.hashSync('p@ssword!23', 10), 
     },
+    {
+        username: 'jane_day',
+        email: 'janeDay@example.com',
+        password: bcrypt.hashSync('@pple!23', 10),
+    },
+    {
+        username: 'mike_smith',
+        email: 'mikeSmith@example.com',
+        password: bcrypt.hashSync('projecT432', 10),
+    },
+    {
+        username: 'sara_bill',
+        email: 'saraBill@example.com',
+        password: bcrypt.hashSync('mond@y!32', 10),
+    },
 ];
 
 const seedUser = async () => User.bulkCreate(userData);
