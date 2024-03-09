100f2b2f0cab23b5e86454af56887d21dfc6e90a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-09 13:51:53 -0500

    Only need the user model thats all

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const sequelize = require("../config/connection");
+const { User } = require("../models");
 
 // GET method for homepage route to render hompage views
 router.get("/", async (req, res) => {
