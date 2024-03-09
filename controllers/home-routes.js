b5e1506e8279256d608586e9799e8a2d5f3f6831
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-09 13:30:15 -0500

    Adding basic user redirect if they are already logged in and made homepage route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,16 +2,45 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { User, Listing, City, BuildingType, ListingType } = require("../models");
 
-// GET method for homepage route and render hompage views
+// GET method for homepage route to render hompage views
 router.get("/", async (req, res) => {
-  res.render("homepage", {
-    logged_in: req.session.logged_in,
-  });
+  try {
+    console.log(req.session.logged_in);
+
+    // Render homepage with login status
+    res.render("homepage", { logged_in: req.session.logged_in });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get("/login", async (req, res) => {});
+router.get("/login", async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      res.redirect("/").reload();
+      return;
+    }
 
-router.get("/signup", async (req, res) => {});
+    res.render("login");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/signup", async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      res.redirect("/").reload();
+      return;
+    }
+
+    res.render("signup");
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 // GET method for logout route
 router.get("/logout", async (req, res) => {
