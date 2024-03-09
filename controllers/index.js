0823ee17a4107eca5e46a1dbc18c166d3ef9ad75
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-09 13:50:44 -0500

    Uncommeted api routes so "logged_in" is available

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 // imports express router pkg + define routes for the app
 const router = require("express").Router();
 
-// const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 
 router.use("/", homeRoutes);
-// router.use("/api", apiRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
