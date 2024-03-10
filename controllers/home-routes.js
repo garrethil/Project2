c4b676e465c32ec2d7dede0afc92d4fa8a8c31da
garrethil <garrethildebrandt@gmail.com>
2024-03-10 11:46:52 -0400

    update home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const sequelize = require("../config/connection");
+const { User, Listing, City, BuildingType, ListingType } = require("../models");
 
 // GET method for homepage route to render hompage views
 router.get("/", async (req, res) => {
-  try {
-    console.log(req.session.logged_in);
+ try { res.render("homepage", {
+    logged_in: req.session.logged_in,
+  });
+
 
     // Render homepage with login status
     res.render("homepage", { logged_in: req.session.logged_in });
-  } catch (err) {
+ } catch (err) {
     console.error(err);
     res.status(500).json(err);
   }
@@ -34,12 +37,10 @@ router.get("/signup", async (req, res) => {
       res.redirect("/").reload();
       return;
     }
-
-    res.render("signup");
   } catch (err) {
-    console.error(err);
-  }
-});
+      res.status(500).json(err);
+    }
+})
 
 // GET method for logout route
 router.get("/logout", async (req, res) => {
@@ -50,5 +51,103 @@ router.get("/logout", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+
+//get all cities to select a city of choice when making a listing or browsing listings
+
+get.router("/cities", async (req, res) => {
+    try {
+        const cityNames = await City.findAll()  
+    
+        const cityName = cityNames.map((type) => type.get({ plain: true }));
+        res.render("locationspage", { cityName })
+           
+        res.status(200).json(cityData);
+    } 
+        catch (err) {
+            res.status(500).json(err);
+    }
+});
+
+// get all building types
+
+get.router("/buildingTypes", async (req, res) => {
+    try {
+        const buildingTypeData = await BuildingType.findAll()   
+    
+        const buildingTypes = buildingTypeData.map((type) => type.get({ plain: true }));
+        res.render("buildingtypepage", { buildingTypes })
+
+        res.status(200).json(buildingTypeData);
+    } 
+        catch (err) {
+            res.status(500).json(err);
+    }
+});
+
+
+
+// get all listing types
+
+get.router("/listingTypes", async (req, res) => {
+    try {
+        const listingTypeData = await ListingType.findAll()   
+    
+        const listingTypes = listingTypeData.map((type) => type.get({ plain: true }));
+        res.render("listingtypepage", { listingTypes })
+        res.status(200).json(listingTypeData);
+    } 
+        catch (err) {
+            res.status(500).json(err);
+    }
+});
+
+// get all Listings
+
+router.get("/listings", async (req, res) => {
+    try {
+        const listingData = await Listing.findAll({
+            include: [{ model: City, ListingType, BuildingType, User }]
+        });
+
+        const listings = listingData.map((listing) => listing.get({ plain: true }));
+
+
+        res.render("listingspage", { listings })
+        res.status(200).json(listingData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// get listing by id
+router.get("/listings/:id", async (req, res) => {
+    try {
+      const listingData = await Listing.findByPk(req.params.id, {
+        include: [{ model: City, ListingType, BuildingType, User }]
+      });
+  
+      if (!listingData) {
+        res.status(404).json({ message: "No listing found with that id!"})
+        return;
+      }
+
+      const listingInfo = listingData.map((listing) => listing.get({ plain: true }));
+  
+      res.render("listing", { listingInfo } )
+  
+      res.status(200).json(listingData);
+    } catch (err) {
+      res.status(500).json(err);
+      
+    }
+  });
+
+  
+
+
+
+
 
 module.exports = router;
