d0a40917e6b97b4cc32c84559584912479b17bbf
garrethil <garrethildebrandt@gmail.com>
2024-03-11 19:54:33 -0400

    format various files

--- seeds/buildingTypeData.js
+++ seeds/buildingTypeData.js
@@ -1,27 +1,27 @@
-const {BuildingType} = require('../models');
+const { BuildingType } = require("../models");
 
 const buildingTypeData = [
-    {
-        building_type: 'House',
-    },
-    {
-        building_type: 'Appartment',
-    },
-    {
-        building_type: 'Cottage',
-    },
-    {
-        building_type: 'TownHouse',
-    },
-    {
-        building_type: 'Bungalow',
-    },
-    {
-        building_type: 'Condo',
-    },
-    {
-        building_type: 'Retail Space',
-    },
+  {
+    building_type: "House",
+  },
+  {
+    building_type: "Appartment",
+  },
+  {
+    building_type: "Cottage",
+  },
+  {
+    building_type: "TownHouse",
+  },
+  {
+    building_type: "Bungalow",
+  },
+  {
+    building_type: "Condo",
+  },
+  {
+    building_type: "Retail Space",
+  },
 ];
 
 const seedBuildingType = async () => BuildingType.bulkCreate(buildingTypeData);
