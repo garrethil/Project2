552276b0da18adeb8b624e1ff9e49069036cf2d1
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-12 07:49:46 -0400

    Fixed som issues with file

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,23 +1,20 @@
 // Function to handle user logout
 const logout = async () => {
   // Send a POST request to the logout endpoint
-  const response = await fetch("/api/users/logout", {
+  const response = await fetch("api/users/logout", {
     method: "POST",
-    headers: { "content-type": "application/json" }, // Set request headers
+    headers: { "Content-Type": "application/json" },
   });
 
   // Check if the response is successful
   if (response.ok) {
     // Redirect the user to the login page if logout is successful
-    document.location.replace("/").reload();
+    document.location.replace("/");
   } else {
     // Display an alert with the error message if logout fails
     alert(response.statusText);
   }
 };
-
-// Log a message for testing purposes
-console.log("this is for logout test");
 
 // Add event listener to the logout button to trigger the logout function
 document.querySelector("#logout").addEventListener("click", logout);
